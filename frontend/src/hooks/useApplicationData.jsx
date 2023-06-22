881abb083ec3eeb508df3779fe83d90ba9280429
Karma Mann <129702902+bavyn@users.noreply.github.com>
2023-06-21 18:01:21 -0700

    removed extra comments

--- frontend/src/hooks/useApplicationData.jsx
+++ frontend/src/hooks/useApplicationData.jsx
@@ -79,45 +79,31 @@ export default function useApplicationData() {
   const [state, dispatch] = useReducer(reducer, appInitialState);
 
   // favourite photos
-  //const [favePhotos, setFavePhotos] = useState([]);
   const toggleFave = (photoId) => {
     // check if already fave
     if (state.favPhotoIds.includes(photoId)) {
       // unfavourite
       return dispatch({ type: ACTIONS.FAV_PHOTO_REMOVED, payload: { photo: photoId } });
-      // const filtered = favePhotos.filter((favePhotos) => favePhotos !== photo);
-      // return setFavePhotos(filtered);
     }
     // favourite
     return dispatch({ type: ACTIONS.FAV_PHOTO_ADDED, payload: { photo: photoId } });
-    // setFavePhotos([...favePhotos, photo]);
-    // console.log('fave button clicked for photo:', photo);
   };
 
-  // toggle favouriting the photo
-  // const handleToggleFave = (id) => toggleFave(id);
-
   // modal
-  // const [modal, setModal] = useState(null);
-
   const openModal = (photoId) => {
-    // setModal(photoId);
-    // console.log('photo clicked', photoId);
     const findPhoto = photos.find(photo => photo.id === photoId);
     dispatch({ type: ACTIONS.SELECT_PHOTO, payload: { photo: findPhoto } });
     dispatch({ type: ACTIONS.DISPLAY_PHOTO_DETAILS, payload: { display: true } });
   };
 
-  // const closeModal = () => setModal(null);
   const closeModal = () => {
     dispatch({ type: ACTIONS.SELECT_PHOTO, payload: { photo: null } });
     dispatch({ type: ACTIONS.DISPLAY_PHOTO_DETAILS, payload: { display: false } });
   };
 
-  // define selectedPhoto for modal
   const selectedPhoto = photos.find((photo) => photo.id === state.selectedPhoto);
 
-  // not using these yet
+  // other
   const setPhotoData = (photos) => {
     dispatch({ type: ACTIONS.SET_PHOTO_DATA, payload: { photos } });
   };
