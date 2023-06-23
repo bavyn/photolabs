df6454231b6e92bd786a8ae3dbd9382545c8b07a
Karma Mann <129702902+bavyn@users.noreply.github.com>
2023-06-22 17:06:48 -0700

    implemented count fave feature

--- frontend/src/components/FavBadge.jsx
+++ frontend/src/components/FavBadge.jsx
@@ -3,10 +3,23 @@ import FavIcon from './FavIcon';
 
 import '../styles/FavBadge.scss';
 
-export const FavBadge = ({ isFavPhotoExist }) => {
+const FavBadge = (props) => {
+
+  const favCount = props.favePhotos.length;
+
   return (
     <div className='fav-badge'>
-      <FavIcon width={20} height={17} fill="#C80000" displayAlert={!!isFavPhotoExist} />
+        <FavIcon
+          width={20}
+          height={17}
+          fill="#C80000"
+          displayAlert={!!props.isFavPhotoExist}
+        />
+
+      <div className='fav-badge__count'>
+        <span>{favCount}</span>
+      </div>
+
     </div>
   );
 };
